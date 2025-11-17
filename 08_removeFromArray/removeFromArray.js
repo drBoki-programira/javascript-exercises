843b2cddd3f7f0832b459212f46a8c0839fb9c12
Boki <bojan.sejnjanovic@gmail.com>
2025-11-17 19:33:16 +0100

    done 1-11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    for (const item of toRemove) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            if (array[i] === item) array.splice(array.indexOf(item), 1)
+        }   
+    }
+    return array
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let result = 0
 
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) return 'ERROR'
+    
+    if (a > b) [a, b] = [b, a]
+    
+    for (a; a <= b; a++) {
+        result += a
+    }
+    
+    return result 
 };
 
 // Do not edit below this line
